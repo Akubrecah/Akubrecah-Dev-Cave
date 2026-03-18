9efbea64093ab1cc695d0b6c059a54b60948583c
Akubrecah <poweldayck@gmail.com>
2026-03-18 12:12:45 +0300

    fix: include pkcs7 and expand certificate properties in node-forge types

--- types/node-forge.d.ts
+++ types/node-forge.d.ts
@@ -14,6 +14,10 @@ declare namespace nodeForge {
       };
       serialNumber: string;
       isIssuer(cert: Certificate): boolean;
+      siginfo?: {
+        algorithmOid: string;
+      };
+      signatureOid?: string;
     }
     interface PrivateKey {
       [key: string]: any;
@@ -24,10 +28,25 @@ declare namespace nodeForge {
     function privateKeyFromPem(pem: string): PrivateKey;
     function certificateFromAsn1(asn1: any): Certificate;
   }
+  namespace asn1 {
+    function fromDer(der: string | Uint8Array): any;
+    function toDer(asn1: any): any;
+  }
+  namespace pkcs12 {
+    function pkcs12FromAsn1(asn1: any, password?: string): any;
+    function toPkcs12Asn1(key: any, certs: any[], password: string, options?: any): any;
+  }
+  namespace pkcs7 {
+    function messageFromAsn1(asn1: any): any;
+  }
+  namespace util {
+    function createBuffer(data?: any, encoding?: string): any;
+  }
   const pki: typeof pki;
-  const asn1: any;
-  const pkcs12: any;
-  const util: any;
+  const asn1: typeof asn1;
+  const pkcs12: typeof pkcs12;
+  const pkcs7: typeof pkcs7;
+  const util: typeof util;
 }
 
 declare module 'node-forge' {
