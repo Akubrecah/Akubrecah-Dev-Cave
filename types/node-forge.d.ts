fedd35b712bb11442597af3f4b6afd0cc65d59c9
Akubrecah <poweldayck@gmail.com>
2026-03-18 10:36:52 +0300

    fix: refine node-forge types to support namespace usage

--- types/node-forge.d.ts
+++ types/node-forge.d.ts
@@ -1,1 +1,35 @@
-declare module 'node-forge';
+declare namespace nodeForge {
+  namespace pki {
+    interface Certificate {
+      [key: string]: any;
+      subject: {
+        getField(name: string): any;
+      };
+      issuer: {
+        getField(name: string): any;
+      };
+      validity: {
+        notBefore: Date;
+        notAfter: Date;
+      };
+      serialNumber: string;
+      isIssuer(cert: Certificate): boolean;
+    }
+    interface PrivateKey {
+      [key: string]: any;
+    }
+    const oids: any;
+    function certificateFromPem(pem: string): Certificate;
+    function decryptRsaPrivateKey(pem: string, password?: string): PrivateKey;
+    function privateKeyFromPem(pem: string): PrivateKey;
+    function certificateFromAsn1(asn1: any): Certificate;
+  }
+  const pki: typeof pki;
+  const asn1: any;
+  const pkcs12: any;
+  const util: any;
+}
+
+declare module 'node-forge' {
+  export = nodeForge;
+}
